538c27a50b46c7cb91497e27e8ef7c84ebad24f1
Moobesoor <edorb721@hotmail.com>
2018-11-19 18:04:16 +0100

    lll

---
+++ Assets/Scripts/NavigateToScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigateToScene : MonoBehaviour {
+
+    [SerializeField]
+
+    string SceneToLoad;
+
+	public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
